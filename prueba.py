ab671f3f91cc71396ffa8be2530b98ea11bf8503
Juan Jose Garcia Perez <juan_jgarciap@soy.sena.edu.co>
2024-09-17 12:38:31 -0500

    si

--- prueba.py
+++ prueba.py
@@ -8,3 +8,29 @@ print(type("hola"))
 print(type(4))
 print(type(5.5))
 print("pruebagitignore")
+print("----------------------------------------------------------------")
+bool = True
+print(bool)
+print(type(bool))
+
+numero = 5
+texto = "hola"
+suma = str(numero) + texto
+print(suma)
+
+bool1 = True
+bool2 = False
+
+suma2 = bool1 + bool2
+print(suma2)
+
+A = True
+B = True
+C = True
+D = True
+
+Resultado = ((A and B)and C)or D
+print(Resultado)
+
+resultado2 = (A and B and C and D)
+print("el resultado 2 es : ", resultado2)
